5cfbc9b43459734e516f340fedac8a127dc886a4
juanjo113 <juanjo113@gmail.com>
2025-08-13 08:44:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-<contenido de app.py aquí>
+from flask import Flask
+from models import get_vehicles_due_today
+from email_utils import send_alert_email
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Vehículos activo"
