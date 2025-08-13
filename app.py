e870113345f3fa477e3ad1b5e45ac59f152c9538
juanjo113 <juanjo113@gmail.com>
2025-08-13 12:13:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import get_vehicles_due_today
 from email_utils import send_email
 from datetime import datetime
