33c65729c711d61747d7b5f0e94bf854591d3c7d
juanjo113 <juanjo113@gmail.com>
2025-08-13 12:45:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde un archivo .env
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-change-me')
